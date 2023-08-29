eb8bb17712c0ce8c37a8c875da00bb97e88466cf
Planxnx <planxthanee@gmail.com>
2023-08-30 05:30:20 +0700

    feat: support deploy branch

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 export const onPostBuild = async function ({ inputs, constants, netlifyConfig, utils: { run, build, status } }) {
   try {
     const { BRANCH, CLOUDFLARE_PAGES_PROJECT_NAME } = netlifyConfig.build.environment;
-    const command = `${inputs.package_exec} wrangler pages deploy ${constants.PUBLISH_DIR} --project-name=${CLOUDFLARE_PAGES_PROJECT_NAME} --branch=${BRANCH}`;
+    const branch = inputs.deploy_target_branch || BRANCH;
+
+    const command = `${inputs.package_exec} wrangler pages deploy ${constants.PUBLISH_DIR} --project-name=${CLOUDFLARE_PAGES_PROJECT_NAME} --branch=${branch}`;
     await run.command(command);
+
     status.show({
       summary: "Deployed to Cloudflare Pages Successfully",
-      text: `Deployed to Pages/${CLOUDFLARE_PAGES_PROJECT_NAME}/${BRANCH}`,
+      text: `Deployed to Pages/${CLOUDFLARE_PAGES_PROJECT_NAME}/${branch}`,
     });
   } catch (error) {
     build.failBuild("Failed while deploy your site to Cloudflare Pages", { error });
